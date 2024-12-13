4155da07ff15a4aa9fc3a4999017b2764de2aeff
aadhiavi <aadhiavi57@outlook.com>
2024-12-13 06:43:49 +0530

    frontend url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (!fs.existsSync(uploadDir)) {
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'https://suhanadtour.netlify.app/',
     credentials: true,
 }));
 
